820dbb097d49f44bdb8bdbd7256578c2be2aa079
Yura09823 <yuranamachinskiy@gmail.com>
2022-12-26 12:23:58 +0200

    add new changes

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+const menuBtnBurger = document.querySelector('.menu-btn__burger');
